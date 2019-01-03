d33ef533322d7fdcc537a8af734c3d069228d5d6
knh369 <knh369@konyadh.gov.tr>
2019-01-03 14:51:05 +0300

    daily edu

--- app.js
+++ app.js
@@ -4,6 +4,11 @@
 const express = require('express');
 const app = express();
 
+// json lar ile ilgileniyoruz
+const jsonParser = require('body-parser').json; // sahip olduğu yeteneklerden json ile çalışmak istedik
+app.use(jsonParser()); // bundan sonraki tüm req ler işlendiğinde gövdesi json olarak parse lanacaktır, req.body den ulaşılabilir, süper
+
+/* MİDDLEWARE SHOWCASE
 app.use((req, res, next) => { // Route tanımlanmadığı için her şartta çalışır
     console.log("Middleware, ilk örneği");
     req.benimMesaj = "Selam 2 no'lu MW"; // 1 de oluşturup 2 de kullanabilmemize bir örnek
@@ -20,6 +25,7 @@ app.use((req, res, next) => { // Route tanımlanmadığı için her şartta çal
     console.log("Bu sınavdan alabileceğin not olsa olsa", req.query.not); // " localhost:3000?not=pekiyi " adresi örneğinde kullandık
     next();
 });
+*/
 
 // port u kuruyoruz. Üretim ortamında olmazsa eğer port olarak 3000'i değerlendirecektir
 const port = process.env.PORT || 3000;
