e6b56ca2fa8db0d4a1b13027be84b366ae9c9f6e
knh369 <knh369@konyadh.gov.tr>
2019-01-09 16:28:08 +0300

    midweek edu

--- mongoose_sandbox.js
+++ mongoose_sandbox.js
@@ -15,11 +15,11 @@ db.once("open", () => { // db.on olay her cerayan ettiğinde çalışırken, db.
     // Veritabanına ilişkin bütün iletişim buraya lütfen.
     const Schema = mongoose.Schema; // Şema için açılışı yaptık
     const AnimalSchema = new Schema({ // Şemayı çizdik.
-        type: String,
-        size: String,
-        color: String,
-        mass: Number,
-        name: String
+        type: {type: String, default: "goldfish"}, //default değerler de atadık
+        size: {type: String, default: "small"},
+        color: {type: String, default: "golden"},
+        mass: {type: Number, default: "0.007"},
+        name: {type: String, default: "Angela"}
     });
 
     const Animal = mongoose.model("Animal", AnimalSchema); // Şemayı isimlendirdik. Veritabanına koleksiyon olarak kaydedildiğinde çoğaltarak isimlendirir. Bizim örneğimizde "Animals"
@@ -32,11 +32,35 @@ db.once("open", () => { // db.on olay her cerayan ettiğinde çalışırken, db.
         name: "Kutay"
     });
 
-    elephant.save((err) => {  // Demez isek kaydetmez
-        if (err) console.error("Kayıt başarısız.", err);
-        else console.log("Kaydettim!");
-        db.close(() => { // "Bitince kapatalım" komutu. Kayıt işlemi bitmeden önce kapatmasın diye kayıt işlemine dahil ettik, son sıraya girsin.
-            console.log("Bağlantıyı kapattım hadi..")
-        }); 
+    const animal = new Animal({}); // default değerleri tetiklemek için kodu yerleştirdik
+
+    const whale = new Animal({ // default un üzerine yazma testi amacıyla. color belirtmedik, default tan alsın.
+        type: "whale",
+        size: "büyük",
+        mass: 190500,
+        name: "Enes"
     });
+
+    Animal.remove({}, (err) => { // tekrar kaydetmesin diye önce siliyoruz. querry object ile neyi sileceğini belirtebiliriz fakat burada herşeyi silsin istedik.
+        // silme işleminden sonra başlasın diye "elephant.save()" fonksiyonunun hepsini buraya taşıdık
+        if (err) console.error(err);
+        elephant.save((err) => {  // Demez isek fil imizi kaydetmez
+            if (err) console.error(err);
+            animal.save((err) => { // default u buraya yerleştirdik, senkronizasyon dışı bir işlem yapmasın diye
+                if (err) console.error(err);
+                whale.save((err) => { // default üzerine yazacak şekilde balina mızı buraya yerleştirdik
+                    if (err) console.error(err);
+                    Animal.find({size: "büyük"}, (err, animals) => { // ilk parametre: sonuçlarla eşleşecek anahtar ve değerleri içerir. ikinci parametre hata ve arama sonuçlarını içerir
+                        animals.forEach((animal) => {
+                            if (err) console.error(err); // Bu yoktu, ben ekledim
+                            console.log(animal.name + " the " + animal.color + " " + animal.type);
+                        });
+                        db.close(() => { // "Bitince kapatalım" komutu. Kayıt işlemi bitmeden önce kapatmasın diye kayıt işlemine dahil ettik, son sıraya girsin.
+                            console.log("Bağlantıyı kapattım hadi..");
+                        });
+                    });
+                });        
+            }); 
+        });
+    }); 
 }); 
